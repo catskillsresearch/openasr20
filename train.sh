044518818bf72c9fd6b3d4596130918bdaba6963
Lars Ericson <lars.ericson@catskillsresearch.com>
2020-09-01 23:13:10 -0400

    Training Amharic with Tensorboard on end2end-asr-pytorch baseline model

---
+++ train.sh
@@ -0,0 +1,26 @@
+export PYTHONPATH=/home/catskills/Desktop/openasr20/end2end_asr_pytorch
+
+python end2end_asr_pytorch/train.py \
+	--train-manifest-list am_train.csv \
+	--valid-manifest-list am_valid.csv \
+	--test-manifest-list am_test.csv \
+	--cuda \
+	--batch-size 8 \
+	--labels-path amharic_characters.json  \
+	--lr 1e-4 \
+	--name amharic_end2end_asr_pytorch_drop0.1_cnn_batch12_4_vgg_layer4 \
+	--save-folder save/ \
+	--save-every 1 \
+	--feat_extractor vgg_cnn \
+	--dropout 0.1 \
+	--num-layers 4 \
+	--num-heads 8 \
+	--dim-model 512 \
+	--dim-key 64 \
+	--dim-value 64 \
+	--dim-input 161 \
+	--dim-inner 2048 \
+	--dim-emb 512 \
+	--shuffle \
+	--min-lr 1e-6 \
+	--k-lr 1
