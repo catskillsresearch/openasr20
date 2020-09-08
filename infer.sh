5f5752c7e284eaa5e7a1bf92eafb01c80b8620c3
Lars Ericson <lars.ericson@catskillsresearch.com>
2020-09-07 20:00:20 -0400

    Reorganize file locations

--- infer.sh
+++ infer.sh
@@ -2,8 +2,8 @@ export PYTHONPATH=/home/catskills/Desktop/openasr20/end2end_asr_pytorch
 
 python end2end_asr_pytorch/infer.py \
 	--continue-from save/${language}_end2end_asr_pytorch_drop0.1_cnn_batch12_4_vgg_layer4/best_model.th \
-	--test-manifest-list DEV_${language}_split.csv \
-	--batch-size 2 \
-	--output RESULT_${language}.txt \
+	--test-manifest-list analysis/${language}/DEV_${language}_split.csv \
+	--batch-size ${batch_size} \
+	--output analysis/${language}/RESULT_${language}.txt \
 	--cuda \
 	--verbose
