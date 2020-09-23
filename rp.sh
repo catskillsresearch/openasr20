533d2a693bef2dbe3feaac67a72c0a339779e2f0
Lars Ericson <lars.ericson@catskillsresearch.com>
2020-09-23 00:40:42 -0400

    Start a rethink

--- rp.sh
+++ rp.sh
@@ -1,3 +1,3 @@
 export PYTHONPATH=/home/catskills/Desktop/openasr20/end2end_asr_pytorch
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 python train_pashto_gpu1.py
--- train_pashto_gpu1.py
+++ train_pashto_gpu1.py
@@ -1,9 +1,9 @@
 from config import C
 from train_language import train_language
 C.language='pashto'
-C.batch_size=6
-C.save_every = 25
-C.start_from = 15
+C.batch_size=12
+C.save_every = 5
+C.start_from = 126
 C.update()
 train_language(C)
 
