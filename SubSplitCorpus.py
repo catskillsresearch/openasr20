6e28cbf9b112bd009c9a4b91b717f404a06fbb40
Lars Ericson <lars.ericson@catskillsresearch.com>
2020-10-03 19:14:16 -0400

    clooossseeeer

--- SubSplitCorpus.py
+++ SubSplitCorpus.py
@@ -4,7 +4,7 @@ from split_artifact import split_artifact
 
 class SubSplitCorpus(SplitCorpus):
     
-    def __init__(self, _corpus, min_words=5, threshold=0.5):
+    def __init__(self, _corpus, min_words=2):
         C = _corpus.C
         df = _corpus.sample_statistics()
         median_words_in_sample=int(df[(df.Corpus=="Split Transcription") &
@@ -15,7 +15,7 @@ class SubSplitCorpus(SplitCorpus):
         problems=[]
         for artifact in tqdm.notebook.tqdm(_corpus.artifacts):
             try:
-                R.append(split_artifact(artifact, min(min_words, median_words_in_sample), threshold))
+                R.append(split_artifact(artifact, min(min_words, median_words_in_sample)))
             except:
                 problems.append(artifact)
         split_artifacts = [item for sublist in R for item in sublist]
--- split_artifact.py
+++ split_artifact.py
@@ -3,14 +3,13 @@ from AudioTextSample import AudioTextSample
 from aggressive_clip_ends import aggressive_clip_ends
 from optimal_split import optimal_split
 
-def split_artifact(artifact, median_words_in_sample, threshold, window=500):
+def split_artifact(artifact, median_words_in_sample, window=500):
     config = artifact.target.C
     if artifact.target.n_words <= median_words_in_sample:
         return [artifact]
     if artifact.source.n_samples < 3*config.sample_rate:  # Under 3 seconds is fine
         return [artifact]
-    audio=artifact.source.value
-    A=optimal_split(0.0001, 0.6, artifact.target.n_words, artifact.target.value, window=window)
+    A=optimal_split(0.0001, 0.6, artifact.target.n_words, artifact.source.value, window=window)
     n_chunks=len(A)
     tokens=artifact.target.tokens
     n_tokens=len(tokens)
